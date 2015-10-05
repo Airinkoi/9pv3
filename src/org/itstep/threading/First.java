6123a7059fc81d670dd74a7f6faecd8787ed1cb9
Вадим <guts.vadim@gmail.com>
2015-10-05 10:06:23 +0300

    Thread

--- src/org/itstep/threading/First.java
+++ src/org/itstep/threading/First.java
@@ -1,7 +1,60 @@
 package org.itstep.threading;
 
-/**
- * Created by Вадим on 05.10.2015.
- */
 public class First {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new CounterRunnable());
+        thread.start();
+    }
+}
+
+class CounterRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                Thread.currentThread().sleep(100);
+                System.out.println("Runnable counter: " + i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class CounterThread extends Thread {
+    private int from, to;
+
+    CounterThread(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public void run() {
+        for (int i = this.from; i < this.to; i++) {
+            try {
+                Thread.currentThread().sleep(100);
+                System.out.println(i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public void setFrom(int from) {
+        this.from = from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public void setTo(int to) {
+        this.to = to;
+    }
 }
