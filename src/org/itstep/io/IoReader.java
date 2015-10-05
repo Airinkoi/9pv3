46aeb90c3595bb1e30cfc7fc1c6c6664afe8cd97
Вадим <guts.vadim@gmail.com>
2015-10-05 09:54:36 +0300

    IO

--- src/org/itstep/io/IoReader.java
+++ src/org/itstep/io/IoReader.java
@@ -1,7 +1,25 @@
 package org.itstep.io;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by Вадим on 05.10.2015.
  */
 public class IoReader {
+    public static void main(String[] args) {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.println("Enter message1: ");
+            String str1 = bf.readLine();
+            System.out.println("Enter message2: ");
+            String str2 = bf.readLine();
+            System.out.println("Message1: " + str1);
+            System.out.println("Message2: " + str2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
