aadd4d0499edcf451c6dd90fd9eb2ecc3406da01
Вадим <guts.vadim@gmail.com>
2015-10-07 09:40:21 +0300

    Socket

--- src/org/itstep/sock/client/Client.java
+++ src/org/itstep/sock/client/Client.java
@@ -1,7 +1,17 @@
 package org.itstep.sock.client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 /**
  * Created by Вадим on 07.10.2015.
  */
 public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket server = new Socket("localhost", 5555);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
