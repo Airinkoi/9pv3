ef7e5dd1452d519016fa1fb789030afe84f7924b
Вадим <guts.vadim@gmail.com>
2015-10-07 11:01:10 +0300

    Socket

--- src/org/itstep/sock/server/Server.java
+++ src/org/itstep/sock/server/Server.java
@@ -3,8 +3,11 @@ package org.itstep.sock.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
+    private static ArrayList<ClientSocketListener> clients = new ArrayList();
+
     public static void main(String[] args) {
         try {
             ServerSocket ss = new ServerSocket(5555);
@@ -12,10 +15,18 @@ public class Server {
                 System.out.println("Waiting for client");
                 Socket clientSocket = ss.accept();
                 ClientSocketListener listener = new ClientSocketListener(clientSocket);
+                clients.add(listener);
                 System.out.println("New user connected");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private static void sendToAll(String message) {
+        for(ClientSocketListener client : clients) {
+            client.send(message);
+        }
+    }
+
 }
