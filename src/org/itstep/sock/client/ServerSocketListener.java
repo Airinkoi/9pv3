8595038951b492f776259b7e293be442dec37556
Вадим <guts.vadim@gmail.com>
2015-10-07 10:55:25 +0300

    Socket

---
+++ src/org/itstep/sock/client/ServerSocketListener.java
@@ -0,0 +1,30 @@
+package org.itstep.sock.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ServerSocketListener extends Thread {
+
+    private Socket serverSocket;
+
+    public ServerSocketListener(Socket serverSocket) {
+        this.serverSocket = serverSocket;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("Listening for client");
+            BufferedReader in = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+            while(true) {
+                String text = in.readLine();
+                System.out.println(text);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
